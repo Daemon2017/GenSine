53beb219761a441bbef4180060412a11f34b5dca
Dmitrij Rusakov <Daemon2017@gmail.com>
2016-03-31 00:12:38 +0300

    1st

---
+++ Sine/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Sine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(94, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(778, 436);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(76, 381);
+            this.listBox1.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(593, 101);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Sine/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sine
+{
+    public partial class Form1 : Form
+    {
+        int X = 0, Y = 127, X_old = 0, Y_old = 127, line = 127;
+        byte[] sinus;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (X < 24)
+            {
+                Y = (int)(127 + 127 * Math.Sin(Math.PI * X * 0.08695652173));
+                X = X + 1;
+
+                Graphics graph = pictureBox1.CreateGraphics();
+                Pen pen_black = new Pen(Brushes.Black, 2);
+                Pen pen_red = new Pen(Brushes.Red, 1);
+
+                graph.DrawLine(pen_red, 0, (float)(pictureBox1.Size.Height - line), pictureBox1.Size.Width, (float)(pictureBox1.Size.Height - line));
+                graph.DrawLine(pen_black, (float)X_old, (float)(pictureBox1.Size.Height - Y_old), (float)X, (float)(pictureBox1.Size.Height - Y));
+
+                if (X >= pictureBox1.Size.Width)
+                {
+                    pictureBox1.Image = null;
+                    X = 0;
+                }
+
+                X_old = X;
+                Y_old = Y;
+
+                label1.Text = Y.ToString();
+                label2.Text = X.ToString();
+
+                listBox1.Items.Add(Y.ToString());
+                richTextBox1.AppendText(Y.ToString());
+                richTextBox1.AppendText(", ");
+
+            }
+
+            /*
+                for (int i = 0; X <= 255; i++, X++)
+                {
+sinus = new byte[256] 
+{127,130,133,136,139,142,145,148,151,154,157,160,164,166,169,172,175,178,181,184,187,189,192,195, 197,200,202,205,
+207,210,212,214,217,219,221,223,225,227,229,231,232,234,236,237,239,240,242,243, 244,245,246,247,248,249, 250,251,
+251,252,252,253,253,253,253,
+
+253,254,253,253,253,253,252,252,251, 251,250,249,249,248,247,246,245,243,242,241,239,
+238,236,235,233,231,230, 228,226,224,222,220,218, 215,213,211,209,206,204,201,199,196,193,191,188,185,182,180,177,
+174,171,168,165,162,159,156,153, 150,147,144,141,137,134,131,128,
+
+125,122,119,116,112,109,106,103,100,97,94,91,88,
+85,82,79,76, 73,71,68,65,62,60,57,54,52,49,47,44,42,40,38,35,33,31,29,27,25,23,22,20,18,17,15,14, 12,11,10,8,7,6,5,
+4,4,3,2,2,1,1,0,0,0,0,0,0,
+
+0,0,0,0,1,1,2,2,3,4,5,6,7,8,9,10,11,13,14,16,17,19,21,22,24, 26,28,30,32,34,36,39,41,43,46,
+48,51,53,56,58,61,64,66,69,72,75,78,81,84,87,89,93,96, 99,102,105,108,111,114,117,120,123,127};
+
+                    Y = sinus[i];
+
+                    Graphics graph = pictureBox1.CreateGraphics();
+                    Pen pen_black = new Pen(Brushes.Black, 2);
+                    Pen pen_red = new Pen(Brushes.Red, 1);
+
+                    graph.DrawLine(pen_red, 0, (float)(pictureBox1.Size.Height - line), pictureBox1.Size.Width, (float)(pictureBox1.Size.Height - line));
+                    graph.DrawLine(pen_black, (float)X_old, (float)(pictureBox1.Size.Height - Y_old), (float)X, (float)(pictureBox1.Size.Height - Y));
+
+                    if (X >= pictureBox1.Size.Width)
+                    {
+                        pictureBox1.Image = null;
+                        X = 0;
+                    }
+
+                    X_old = X;
+                    Y_old = Y;
+
+                    label1.Text = Y.ToString();
+                    label2.Text = X.ToString();
+                }     
+             */
+        }
+    }
+}
